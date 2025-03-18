55ea677329f2600fb2836a728f24ed739d6348ea
bob4inski <robertstryzhak@yandex.ru>
2025-03-18 21:11:46 +0300

    Remove junk files

--- cats/mini-cat.py
+++ cats/mini-cat.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import requests
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/")
 def hello_world():
-    resp = requests.get('https://api.thecatapi.com/v1/images/search')
-    cat_json = resp.json()
-    
-    if cat_json:
-        return cat_json[0].get('url', '')
-    else:
-        return ''
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# Oh, hello there
